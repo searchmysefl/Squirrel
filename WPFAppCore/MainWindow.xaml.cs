aa6d15d28148690cb33cb8b75e2ab36c92b06dcf
Ivan Semenov <moyiphone5sone@gmail.com>
2022-01-26 18:37:39 +0700

    test

---
+++ WPFAppCore/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+using Squirrel;
+
+namespace WPFAppCore
+{
+    public partial class MainWindow : Window
+    {
+        UpdateManager manager;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            manager = await UpdateManager
+                //.GitHubUpdateManager(@"https://github.com/meJevin/WPFCoreTest");
+                .GitHubUpdateManager(@"https://github.com/searchmysefl/Squirrel");
+
+            CurrentVersionTextBox.Text = manager.CurrentlyInstalledVersion().ToString();
+        }
+
+        private async void CheckForUpdatesButton_Click(object sender, RoutedEventArgs e)
+        {
+            var updateInfo = await manager.CheckForUpdate();
+
+            if (updateInfo.ReleasesToApply.Count > 0)
+            {
+                UpdateButton.IsEnabled = true;
+            }
+            else
+            {
+                UpdateButton.IsEnabled = false;
+            }
+        }
+
+        private async void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            await manager.UpdateApp();
+
+            MessageBox.Show("Updated succesfuly!");
+        }
+    }
+}
